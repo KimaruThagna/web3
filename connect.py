5e8435fdb809bb09ecf49130751fe20073f82b71
Kimaru thagana <macbuntu@192.168.1.8>
2020-12-16 21:40:12 +0300

    check connection

--- connect.py
+++ connect.py
@@ -1,2 +1,4 @@
-import web3
+from web3 import Web3
 infura_url = 'https://mainnet.infura.io/v3/ee3fe0d462ca4294b10ffbf535619190'
+client = Web3(Web3.HTTPProvider(infura_url)) # connect to the ethereum blockchain
+print(client.isConnected()) # check connection
