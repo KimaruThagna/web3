5c04520f79f86d31adc3d86b27aa10045f6d1b2d
Kimaru thagana <macbuntu@localhost.localdomain>
2020-12-18 05:04:53 +0300

    using local blockchain ganache

---
+++ using_ganache.py
@@ -0,0 +1,16 @@
+from web3 import Web3
+from dotenv import load_dotenv
+import os
+load_dotenv('.env')
+ganache_url = os.environ.get('ganache_url')
+
+account_1 = os.environ.get('account_1')
+account_2 = os.environ.get('account_2')
+
+private_key_1 = os.environ.get('private_key_1')
+private_key_2 = os.environ.get('private_key_2')
+
+client = Web3(Web3.HTTPProvider(ganache_url)) # connect to the ethereum blockchain
+print(client.isConnected()) # check connection
+# latest block
+print(client.eth.blockNumber)
