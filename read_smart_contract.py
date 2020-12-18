3daf7d12a5d446c42128bf45430e13b0aef26969
Kimaru thagana <macbuntu@localhost.localdomain>
2020-12-18 04:59:43 +0300

    read balance from smart contract

--- read_smart_contract.py
+++ read_smart_contract.py
@@ -1,4 +1,4 @@
-from web3 import Web3
+from .connect import client, Web3
 from dotenv import load_dotenv
 import json,  os
 load_dotenv('.env')
@@ -6,4 +6,15 @@ load_dotenv('.env')
 abi = json.loads(os.environ.get('abi'))
 address = os.environ.get('address')
 
-smart_contract = Web3.eth.contract(address,abi)
+smart_contract = client.eth.contract(address,abi)
+# token supply in Wei
+total_token_supply = smart_contract.functions.totalSupply().cal()
+print(Web3.fromWei(total_token_supply,'ether')) # in ethers
+# contract name
+print(smart_contract.functions.name().call())
+print(smart_contract.functions.symbol().call())
+# find balance of an account
+balance = smart_contract.functions.balanceOf(address).call()
+print(balance)
+# convert balance tfrom Wei to ether
+print(Web3.fromWei(balance,'ether'))
