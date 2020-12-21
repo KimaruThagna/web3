dc997f5e7baabb4a0eabc3181b81871094dce83c
Kimaru thagana <macbuntu@localhost.localdomain>
2020-12-21 16:37:43 +0300

    deploy contract to blockchain

--- deploy_smart_contract.py
+++ deploy_smart_contract.py
@@ -10,15 +10,7 @@ print(client.eth.blockNumber)
 # designate first account to be default account
 Web3.eth.defaultAccount = Web3.eth.accounts[0]
 
-# instantiate contract
+
 abi = json.loads(os.environ.get('abi'))
 address = Web3.toChecksumAddress(os.environ.get('address'))
 contract_byte_code = os.environ.get('byte_code')
-
-smart_contract = client.eth.contract(address=address,abi=abi)
-print(smart_contract.functions.greet().call())
-# send data to contract
-tx_hash = smart_contract.functions.setGreeting('Hello world from Solidity and Ganache').transact()
-# wait for transaction to be minedd and get receipt
-Web3.eth.waitForTransactionReceipt(tx_hash)
-print(f'New updated greeting {smart_contract.functions.greet().call()}')
