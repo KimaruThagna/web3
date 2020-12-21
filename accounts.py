921eb9cc76bd4f0ce064e1068f21df2919eb38e5
Kimaru thagana <macbuntu@localhost.localdomain>
2020-12-21 17:23:33 +0300

    encrypt account

--- accounts.py
+++ accounts.py
@@ -3,3 +3,9 @@ import os
 infura_url = os.environ.get('infura_url')
 client = Web3(Web3.HTTPProvider(infura_url)) # connect to the ethereum blockchain
 print(client.isConnected()) # check connection
+#create account
+account = client.eth.account.create()
+print(account)
+# the private key is very sensitive and should be stored in encrypted format
+passphrase = 'my passphrase'
+keystore = account.encrypt(passphrase)
