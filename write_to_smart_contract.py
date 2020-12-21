8c810eca5b76dad09225707c477c2d1197b32847
Kimaru thagana <macbuntu@localhost.localdomain>
2020-12-21 16:06:24 +0300

    setup script

--- write_to_smart_contract.py
+++ write_to_smart_contract.py
@@ -3,3 +3,8 @@ from dotenv import load_dotenv
 import os
 load_dotenv('.env')
 ganache_url = os.environ.get('ganache_url')
+client = Web3(Web3.HTTPProvider(ganache_url)) # connect to the ethereum blockchain
+print(client.isConnected()) # check connection
+# latest block
+print(client.eth.blockNumber)
+
