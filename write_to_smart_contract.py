96a354c4898049ae64644400490b47c0d9f113a1
Kimaru thagana <macbuntu@localhost.localdomain>
2020-12-21 16:13:40 +0300

    setup contract

--- write_to_smart_contract.py
+++ write_to_smart_contract.py
@@ -1,6 +1,6 @@
 from web3 import Web3
 from dotenv import load_dotenv
-import os
+import os, json
 load_dotenv('.env')
 ganache_url = os.environ.get('ganache_url')
 client = Web3(Web3.HTTPProvider(ganache_url)) # connect to the ethereum blockchain
@@ -8,3 +8,9 @@ print(client.isConnected()) # check connection
 # latest block
 print(client.eth.blockNumber)
 
+# instantiate contract
+abi = json.loads(os.environ.get('abi'))
+address = Web3.toChecksumAddress(os.environ.get('address'))
+
+smart_contract = client.eth.contract(address=address,abi=abi)
+print(smart_contract.functions.greet().call())
